4a884dfa22e9c8e6a1e5b13a5df89ce0f77f78a0
Xcode <xcode@Senya-MacBook-Pro.local>
2026-02-02 10:36:06 +0300

    [Change] Add grouping for a message reaction [Change] Add grouping for normal message in a thread [Change] Add a feature to show title of the thread or group title name

--- NotificationService/NotificationService.swift
+++ NotificationService/NotificationService.swift
@@ -22,25 +22,25 @@ class NotificationService: UNNotificationServiceExtension {
         let type = request.content.userInfo["requestType"] as? String ?? ""
         if type == "reaction" {
             handleReaction()
-        } else {
-            contentHandler(request.content)
+        } else if let newContent = request.content.mutableCopy() as? UNMutableNotificationContent {
+            newContent.title = bool(key: "isGroup") ? stringValue(forKey: "threadName") ?? "" : stringValue(forKey: "title") ?? ""
+            newContent.threadIdentifier = "threadId-\(int(key: "threadId"))"
+            contentHandler(newContent)
         }
     }
     
     private func handleReaction() {
         
         let mutableContent = request?.content.mutableCopy() as? UNMutableNotificationContent
-        guard
-            let userInfo = request?.content.userInfo,
-            let stickerValue = userInfo["sticker"] as? String,
-            let stickerInt = Int(stickerValue),
-            let content = mutableContent
-        else { return }
+        guard let content = mutableContent else { return }
         
-        let emoji = Sticker(rawValue: stickerInt)?.emoji
+        let sticker = intValue(forKey: "sticker") ?? -1
+        let emoji = Sticker(rawValue: sticker)?.emoji
         content.title = "Reaction"
-        let doer = userInfo["title"] as? String ?? "Someone"
+        let doer = stringValue(forKey: "title") ?? "Someone"
         content.body = "\(doer) has reacted to a message with \(emoji ?? "")"
+        content.categoryIdentifier = "reaction-\(int(key: "threadId"))"
+        content.threadIdentifier = "\(int(key: "threadId"))-\(int(key: "msgId"))"
         content.subtitle = ""
         content.sound = nil
         contentHandler?(content)
@@ -52,5 +52,32 @@ class NotificationService: UNNotificationServiceExtension {
         if let contentHandler = contentHandler, let content = request?.content {
             contentHandler(content)
         }
+    }
+}
+
+extension NotificationService {
+    private func int(key: String) -> Int {
+       intValue(forKey: key) ?? -1
+    }
+    
+    private func bool(key: String) -> Bool {
+        if let stringValue = stringValue(forKey: "isGroup"), let boolValue = Bool(stringValue) {
+            return boolValue
+        }
+        return false
+    }
+    
+    private func intValue(forKey key: String) -> Int? {
+        if let stringValue = request?.content.userInfo[key] as? String, let intValue = Int(stringValue) {
+            return intValue
+        }
+        return nil
+    }
+    
+    private func stringValue(forKey key: String) -> String? {
+        if let stringValue = request?.content.userInfo[key] as? String {
+            return stringValue
+        }
+        return nil
     }
 }
