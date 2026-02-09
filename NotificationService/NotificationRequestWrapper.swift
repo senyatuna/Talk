f11d069c605eb5df74389a629c6854f36c1871a7
Xcode <xcode@Senya-MacBook-Pro.local>
2026-02-09 15:01:30 +0300

    [Change] Add wrapper around the request to move all helper methods out of the service extension class

---
+++ NotificationService/NotificationRequestWrapper.swift
@@ -0,0 +1,138 @@
+//
+//  NotificationRequestWrapper.swift
+//  NotificationService
+//
+//  Created by Xcode on 2/9/26.
+//
+
+import UserNotifications
+
+class NotificationRequestWrapper {
+    let request: UNNotificationRequest
+    
+    init(request: UNNotificationRequest) {
+        self.request = request
+    }
+}
+
+/// Helpers
+extension NotificationRequestWrapper {
+    func int(key: String) -> Int {
+        intValue(forKey: key) ?? -1
+    }
+    
+    func bool(key: String) -> Bool {
+        if let stringValue = stringValue(forKey: key), let boolValue = Bool(stringValue) {
+            return boolValue
+        }
+        return false
+    }
+    
+    func intValue(forKey key: String) -> Int? {
+        if let stringValue = request.content.userInfo[key] as? String, let intValue = Int(stringValue) {
+            return intValue
+        }
+        return nil
+    }
+    
+    func stringValue(forKey key: String) -> String? {
+        if let stringValue = request.content.userInfo[key] as? String {
+            return stringValue
+        }
+        return nil
+    }
+    
+    var isGroup: Bool {
+        bool(key: "isGroup")
+    }
+    
+    func titleWithGroupIconIfIsGroup() -> String {
+        let title = stringValue(forKey: "title") ?? ""
+        return isGroup ? "\(title)" : title
+    }
+    
+    func isReactionType() -> Bool {
+        let type = request.content.userInfo["requestType"] as? String ?? ""
+        return type == "reaction"
+    }
+    
+    func isReply() -> Bool {
+        return intValue(forKey: "repliedToMessageMsgId") != nil
+    }
+    
+    func isEditMessage() -> Bool {
+        let type = request.content.userInfo["requestType"] as? String ?? ""
+        return type == "editMessage"
+    }
+    
+    func requestMessageId(_ request: UNNotificationRequest?) -> Int? {
+        guard let msgId = request?.content.userInfo["msgId"] as? String else { return nil }
+        return Int(msgId)
+    }
+    
+    func repliedToString() -> String? {
+        let localRepliedToString = isRTL ? repliedToNameFA : repliedToNameEN
+        let repliedToName = fullNameReplier() ?? stringValue(forKey: "repliedToMessageSenderUsername") ?? ""
+        return "\(localRepliedToString)\(repliedToName)"
+    }
+    
+    func makeReplyBody() -> String? {
+        return "\(stringValue(forKey: "body") ?? "")"
+    }
+    
+    func fullNameReplier() -> String? {
+        let firstName = stringValue(forKey: "repliedToMessageSenderFirstname")
+        let lastName = stringValue(forKey: "repliedToMessageSenderLastname")
+        guard let firstName = firstName, let lastName = lastName else { return nil }
+        return "\(firstName) \(lastName)"
+    }
+    
+    var groupIconAttachment: UNNotificationAttachment? {
+        guard
+            isGroup,
+            let groupIconURL: URL = Bundle.main.url(forResource: "ic_group", withExtension: "png"),
+            let attachment = try? UNNotificationAttachment(identifier: "groupIcon", url: groupIconURL, options: nil)
+        else { return nil }
+        return attachment
+    }
+    
+    var mutableContent: UNMutableNotificationContent? {
+        request.content.mutableCopy() as? UNMutableNotificationContent
+    }
+    
+    var title: String? {
+        stringValue(forKey: "title")
+    }
+    
+    var threadName: String? {
+        stringValue(forKey: "threadName")
+    }
+    
+    var threadId: Int {
+        int(key: "threadId")
+    }
+    
+    var messageId: Int {
+        int(key: "msgId")
+    }
+    
+    var sticker: Int {
+        intValue(forKey: "sticker") ?? -1
+    }
+}
+
+extension NotificationRequestWrapper {
+    var repliedToNameEN : String { "Replid to: " }
+    var repliedToNameFA: String { "2b7Yp9iz2K4g2KjZhzog".fromBase64() ?? "" }
+    var youEN: String { "you" }
+    var youFA: String { "2LTZhdin".fromBase64() ?? "" }
+    var editedEN: String { "(Edited ✏️)" }
+    var editedFA: String { "KNin2LXZhNin2K0g2LTYr+Kcj++4jyk=".fromBase64() ?? "" }
+    
+    var isRTL: Bool {
+        let groupName = "group.com.lmlvrmedia.leitnerbox"
+        let groupUserDefaults = UserDefaults(suiteName: groupName)
+        let defaultLanguage = groupUserDefaults?.string(forKey: "DefaultGroupLanguage")
+        return defaultLanguage == "ZmFfSVI=".fromBase64() ?? ""
+    }
+}
--- NotificationService/NotificationService.swift
+++ NotificationService/NotificationService.swift
@@ -14,16 +14,19 @@ class NotificationService: UNNotificationServiceExtension {
     
     var contentHandler: ((UNNotificationContent) -> Void)?
     var request: UNNotificationRequest?
+    var wrapper: NotificationRequestWrapper?
     
     override func didReceive(_ request: UNNotificationRequest, withContentHandler contentHandler: @escaping (UNNotificationContent) -> Void) {
         self.contentHandler = contentHandler
         self.request = request
+        let wrapper = NotificationRequestWrapper(request: request)
+        self.wrapper = wrapper
         
-        if isReactionType() {
+        if wrapper.isReactionType() {
             handleReaction()
-        } else if isReply() {
+        } else if wrapper.isReply() {
             handleReply()
-        } else if isEditMessage() {
+        } else if wrapper.isEditMessage() {
             Task { await handleEditMessage() }
         } else {
             handleNormalMessage()
@@ -43,181 +46,59 @@ class NotificationService: UNNotificationServiceExtension {
 extension NotificationService {
     
     private func handleNormalMessage() {
-        guard let content = mutableContent else { return }
-        content.title = isGroup ? threadName ?? "" : title ?? ""
-        content.threadIdentifier = "threadId-\(threadId)"
-        if let groupIconAttachment = groupIconAttachment {
+        guard let wrapper = wrapper, let content = wrapper.mutableContent else { return }
+        content.title = wrapper.isGroup ? wrapper.threadName ?? "" : wrapper.title ?? ""
+        content.threadIdentifier = "threadId-\(wrapper.threadId)"
+        if let groupIconAttachment = wrapper.groupIconAttachment {
             content.attachments = [groupIconAttachment]
         }
         contentHandler?(content)
     }
     
     private func handleReaction() {
-        guard let content = mutableContent else { return }
-        let emoji = Sticker(rawValue: sticker)?.emoji
+        guard let wrapper = wrapper, let content = wrapper.mutableContent else { return }
+        let emoji = Sticker(rawValue: wrapper.sticker)?.emoji
         content.title = "Reaction"
-        let doer = title ?? "Someone"
+        let doer = wrapper.title ?? "Someone"
         content.body = "\(doer) has reacted to a message with \(emoji ?? "")"
-        content.categoryIdentifier = "reaction-\(threadId)"
-        content.threadIdentifier = "\(threadId)-\(messageId)"
+        content.categoryIdentifier = "reaction-\(wrapper.threadId)"
+        content.threadIdentifier = "\(wrapper.threadId)-\(wrapper.messageId)"
         content.subtitle = ""
         content.sound = nil
         contentHandler?(content)
     }
     
     private func handleReply() {
-        guard let content = mutableContent else { return }
-        content.title = titleWithGroupIconIfIsGroup()
-        content.subtitle = repliedToString() ?? ""
-        content.body = makeReplyBody() ?? ""
-        content.threadIdentifier = "threadId-\(threadId)"
+        guard let wrapper = wrapper, let content = wrapper.mutableContent else { return }
+        content.title = wrapper.titleWithGroupIconIfIsGroup()
+        content.subtitle = wrapper.repliedToString() ?? ""
+        content.body = wrapper.makeReplyBody() ?? ""
+        content.threadIdentifier = "threadId-\(wrapper.threadId)"
         
-        if let groupIconAttachment = groupIconAttachment {
+        if let groupIconAttachment = wrapper.groupIconAttachment {
             content.attachments = [groupIconAttachment]
         }
         contentHandler?(content)
     }
     
     private func handleEditMessage() async {
-        let currentRequestMessageId = requestMessageId(request)
+        guard let wrapper = wrapper else { return }
+        let currentRequestMessageId = wrapper.requestMessageId(request)
         let notifs = await UNUserNotificationCenter.current().deliveredNotifications()
-        let notif = notifs.first(where: { requestMessageId($0.request) == currentRequestMessageId })
+        let notif = notifs.first(where: { wrapper.requestMessageId($0.request) == currentRequestMessageId })
         if let request = notif?.request {
             /// Remove current delivered notification
             UNUserNotificationCenter.current().removeDeliveredNotifications(withIdentifiers: [request.identifier])
             
             /// Add new notification
-            if let newContent = mutableContent {
-                newContent.title = isGroup ? threadName ?? "" : title ?? ""
-                newContent.subtitle = isRTL ? editedFA : editedEN
-                newContent.threadIdentifier = "threadId-\(threadId)"
+            if let newContent = wrapper.mutableContent {
+                newContent.title = wrapper.isGroup ? wrapper.threadName ?? "" : wrapper.title ?? ""
+                newContent.subtitle = wrapper.isRTL ? wrapper.editedFA : wrapper.editedEN
+                newContent.threadIdentifier = "threadId-\(wrapper.threadId)"
                 contentHandler?(newContent)
             } else {
                 contentHandler?(request.content)
             }
         }
-    }
-}
-
-/// Helpers
-extension NotificationService {
-    private func int(key: String) -> Int {
-        intValue(forKey: key) ?? -1
-    }
-    
-    private func bool(key: String) -> Bool {
-        if let stringValue = stringValue(forKey: key), let boolValue = Bool(stringValue) {
-            return boolValue
-        }
-        return false
-    }
-    
-    private func intValue(forKey key: String) -> Int? {
-        if let stringValue = request?.content.userInfo[key] as? String, let intValue = Int(stringValue) {
-            return intValue
-        }
-        return nil
-    }
-    
-    private func stringValue(forKey key: String) -> String? {
-        if let stringValue = request?.content.userInfo[key] as? String {
-            return stringValue
-        }
-        return nil
-    }
-    
-    private var isGroup: Bool {
-        bool(key: "isGroup")
-    }
-    
-    private func titleWithGroupIconIfIsGroup() -> String {
-        let title = stringValue(forKey: "title") ?? ""
-        return isGroup ? "\(title)" : title
-    }
-    
-    private func isReactionType() -> Bool {
-        let type = request?.content.userInfo["requestType"] as? String ?? ""
-        return type == "reaction"
-    }
-    
-    private func isReply() -> Bool {
-        return intValue(forKey: "repliedToMessageMsgId") != nil
-    }
-    
-    private func isEditMessage() -> Bool {
-        let type = request?.content.userInfo["requestType"] as? String ?? ""
-        return type == "editMessage"
-    }
-    
-    private func requestMessageId(_ request: UNNotificationRequest?) -> Int? {
-        guard let msgId = request?.content.userInfo["msgId"] as? String else { return nil }
-        return Int(msgId)
-    }
-    
-    private func repliedToString() -> String? {
-        let localRepliedToString = isRTL ? repliedToNameFA : repliedToNameEN
-        let repliedToName = fullNameReplier() ?? stringValue(forKey: "repliedToMessageSenderUsername") ?? ""
-        return "\(localRepliedToString)\(repliedToName)"
-    }
-    
-    private func makeReplyBody() -> String? {
-        return "\(stringValue(forKey: "body") ?? "")"
-    }
-    
-    private func fullNameReplier() -> String? {
-        let firstName = stringValue(forKey: "repliedToMessageSenderFirstname")
-        let lastName = stringValue(forKey: "repliedToMessageSenderLastname")
-        guard let firstName = firstName, let lastName = lastName else { return nil }
-        return "\(firstName) \(lastName)"
-    }
-    
-    private var groupIconAttachment: UNNotificationAttachment? {
-        guard
-            isGroup,
-            let groupIconURL: URL = Bundle.main.url(forResource: "ic_group", withExtension: "png"),
-            let attachment = try? UNNotificationAttachment(identifier: "groupIcon", url: groupIconURL, options: nil)
-        else { return nil }
-        return attachment
-    }
-    
-    private var mutableContent: UNMutableNotificationContent? {
-        request?.content.mutableCopy() as? UNMutableNotificationContent
-    }
-    
-    private var title: String? {
-        stringValue(forKey: "title")
-    }
-    
-    private var threadName: String? {
-        stringValue(forKey: "threadName")
-    }
-    
-    private var threadId: Int {
-        int(key: "threadId")
-    }
-    
-    private var messageId: Int {
-        int(key: "msgId")
-    }
-    
-    private var sticker: Int {
-        intValue(forKey: "sticker") ?? -1
-    }
-}
-
-
-extension NotificationService {
-    private var repliedToNameEN : String { "Replid to: " }
-    private var repliedToNameFA: String { "2b7Yp9iz2K4g2KjZhzog".fromBase64() ?? "" }
-    private var youEN: String { "you" }
-    private var youFA: String { "2LTZhdin".fromBase64() ?? "" }
-    private var editedEN: String { "(Edited ✏️)" }
-    private var editedFA: String { "KNin2LXZhNin2K0g2LTYr+Kcj++4jyk=".fromBase64() ?? "" }
-    
-    private var isRTL: Bool {
-        let groupName = "group.com.lmlvrmedia.leitnerbox"
-        let groupUserDefaults = UserDefaults(suiteName: groupName)
-        let defaultLanguage = groupUserDefaults?.string(forKey: "DefaultGroupLanguage")
-        return defaultLanguage == "ZmFfSVI=".fromBase64() ?? ""
     }
 }
